221e6f29b4cefedd4f9ea80761f0ea9c50bf00b7
Pedro Henrique da Silva <p3drohdasilva@gmail.com>
2022-08-24 19:49:21 +0000

    reqs

--- app.js
+++ app.js
@@ -4,15 +4,29 @@
 // fizzbuzz()
 
 const express = require('express')
-const app = express()
+const TransacoesRepositorio = require("./transacoes-repositorio")
 
+const app = express()
 const port = 3000;
 
+app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
-app.get('/ola', (req , res) => {
-  res.send("ola mundo")
+app.get('/transacoes', (req , res) => {
+  const repositorio = new TransacoesRepositorio
+  const transacoes = repositorio.listarTransacoes()
+
+  res.send(transacoes)
+
 })
+
+app.post('/transacoes', (req, res) => {
+  const repositorio = new TransacoesRepositorio()
+  const transacao = req.body
+  repositorio.criarTransacao(transacao)
+  res.status(201).send(transacao)
+})
+
 
 app.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}`);
